bfc6e21cc4b623b529a78ac03ba4c6d4ffe82cc5
hastekk-ppk <114733030+hastekk-ppk@users.noreply.github.com>
2023-02-13 17:56:19 +0200

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,15 @@
 package domain;
 
+import java.util.Objects;
+
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static boolean ArmstrongNumber(Long R){
+        String num = Long.toString(R);
+        Integer lenght = num.length();
+        Double sum = 0d;
+        for(int i = 0;i<lenght;i++){
+            sum += Math.pow(Long.parseLong(Character.toString(num.charAt(i))), lenght);
+        }
+        return Objects.equals(Double.valueOf(R), sum);
     }
 }
